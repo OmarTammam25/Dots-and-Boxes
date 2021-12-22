a746a576f3554ee756ee57d29aa294955ece5bda
OmarTammam25 <omartammam25@gmail.com>
2021-12-22 17:14:04 +0200

    player struct and color

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <Windows.h>
+#include "header files\colors.h"
+#include "header files\player_info.h"
+#define MAX_NAME 10
 
 int main(){
 // initializing the game
@@ -11,17 +14,17 @@ int main(){
     int num_row = 0; //number of dots in each colomn
     int num_col = 0; //number of dots in each row
     int plyers_num=0;
-    
-    
+
+
     char main_menu [4][40]= {"1- Start a new game","2- Load a game","3- Top ten players","4- Exit"};
     //char conf_menu[3][40] = {"size of grid","one or two players","players configuration"};
     char grid_size[6][40] = {"1- 2*2","2- 3*3","3- 4*4","4- 5*5","5- custom size","6- Back"};
-    char one_two[3][40]   = {"1- one player","2- two players","3- back"}; 
+    char one_two[3][40]   = {"1- one player","2- two players","3- back"};
 
     int choice=0;
     main_page:
         //printing the main menu elements
-        
+
         for (int i=0;i<4;i++){
             printf("%s\n",main_menu[i]);
         }
@@ -32,21 +35,21 @@ int main(){
         switch(choice){
             case 1:
                 system("cls");
-                printf("\t\t\t\tStart a new game"); 
+                printf("\t\t\t\tStart a new game");
                 goto game_conf; // take him to game configration page
                 choice=0;
                 break;
-            case 2: 
+            case 2:
                 system("cls");
                 printf("\t\t\t\tLoad a game"); // take him to load game page
                 choice=0;
                 break;
-            case 3: 
+            case 3:
                 system("cls");
                 printf("\t\t\t\tTop ten players"); // take hime to top ten players
                 choice=0;
                 break;
-            case 4: 
+            case 4:
                 return 0;// Exit the game
                 break;
             default :
@@ -61,7 +64,7 @@ int main(){
         //game setup (size of gride , 2 players or one , choosing colors)
         choice=0;
         printf("\t\t\t\tthe size of the grid\n"); //size of grid menu
-        
+
         for(int j=0;j<6;j++){
             printf("%s\n",grid_size[j]);
         }
@@ -90,19 +93,15 @@ int main(){
                 choice=0;
                 break;
              case 5:
-                //custom input 
-                //take care if the user enterd an invalid input
-                /*
-                do{
-                    num_row =0;
-                    printf("enter a valid num between 1-10");
-                    scanf("%d", &num_row);
-                }while(num_row > 10 || num_row <1);*/
+                //custom input
+                printf("\nEnter number of rows and columns: ");
+                scanf("%d %d", &num_row, &num_col);
+                choice =0;
                 break;
 
             case 6:
                 //go to the main page
-                system("cls"); 
+                system("cls");
                 goto main_page;
             default:
                 printf("\t\t\t\tplease enter a valid number between 1-6");
@@ -115,8 +114,8 @@ int main(){
         system("cls");
         printf("\t\t\t\tgame configuration\n");
         choice=0;
-        printf("\t\t\t\tnumber of players\n");//one or two players
-    
+        printf("\t\t\t\tnumber of players\n"); //one or two players
+
         for(int j=0;j<3;j++){
             printf("%s\n",one_two[j]);
         }
@@ -140,15 +139,60 @@ int main(){
                 choice=0;
                 Sleep(1000);
                 goto players_number;
-        
+
         }
     /*
-    block of code to ask for user name and user color 
+    block of code to ask for user name and user color
     if one user or two users
     add user name to user file and wait for his score if it is his first time with the game
     update the score of the user if it is higher than his last high score
     */
-   
+
+        typedef struct{
+            int score;
+            WORD colorF;
+            WORD colorB;
+            char name[MAX_NAME+1];
+        }player;
+        player player1;
+        player player2;
+        choice = 0;
+
+        system("cls");
+        printf("\t\t\t\tPlayers information\n");
+        /* printf("Please enter your player 1 name: ");
+        scanf("") */ // TODO GET PLAYER NAME
+        printf("Please enter player 1 prefered color:\n");
+        printf("1.Red\n");
+        printf("2.Blue\n");
+
+    color_choice:
+        scanf("%d", &choice);
+        switch(choice){
+            case 1:
+                player1.colorF = FOREGROUND_RED;
+                player1.colorB = BACKGROUND_RED;
+                player2.colorF = FOREGROUND_BLUE;
+                player2.colorB = BACKGROUND_BLUE;
+                break;
+            case 2:
+                player1.colorF = FOREGROUND_BLUE;
+                player1.colorB = BACKGROUND_BLUE;
+                player2.colorF = FOREGROUND_RED;
+                player2.colorB = BACKGROUND_RED;
+                break;
+            default:
+                printf("please enter num 1 or 2: ");
+                goto color_choice;
+        }
+
+        /* setColor(player1.colorB);
+        printf("     \n     ");
+        setColorDefault(); */
+
+        //main game loop:
+
+
 
     return 0;
 }
