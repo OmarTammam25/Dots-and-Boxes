3e4650911052772f30c601120d6dc25ba38dfaea
josephShokry <josephshokry9@gmail.com>
2021-12-22 18:56:35 +0200

    players colors menu
    completing and adding example of the grid function deleting flatten.h and adding its function to grid function

--- header files/lines array.h
+++ header files/lines array.h
@@ -1,4 +1,4 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 // generate array which will hold the lines. dynamic array ;)
 void generateGridArray(int n, int m, int gridArray[n+n-1][m+m-1]){
@@ -26,7 +26,7 @@ void generateGridArray(int n, int m, int gridArray[n+n-1][m+m-1]){
 
 // enter row1 as first row is 1
 // enter col1 as first col is 1
-void addLineToArray(int n, int m, int gridArray[n+n-1][m+m-1],int row1, int row2, int col1, int col2){
+void addLineToArray(int n, int m, int gridArray[n+n-1][m+m-1],int row1, int row2, int col1, int col2,int turn){
 
     if(row1 == row2){
         int col3;
@@ -35,7 +35,7 @@ void addLineToArray(int n, int m, int gridArray[n+n-1][m+m-1],int row1, int row2
         if(gridArray[row1 + row2 -2][2*col3 - 3] == 1){
             printf("ERROR");
         }else{
-            gridArray[row1 + row2 -2][2*col3 - 3] = 1;
+            gridArray[row1 + row2 -2][2*col3 - 3] = 1*turn;
         }
     }
     if(col1 == col2){
@@ -45,19 +45,16 @@ void addLineToArray(int n, int m, int gridArray[n+n-1][m+m-1],int row1, int row2
         if(gridArray[2*row3 - 3][col1 + col2 - 2] == 1){
             printf("ERROR");   // TODO CHECK IN MAIN GAME LOOP 
         }else{
-            gridArray[2*row3 - 3][col1 + col2 - 2] = 1;
+            gridArray[2*row3 - 3][col1 + col2 - 2] = 1*turn;
         }
     }
 }
 //TODO make it a header file and put it in main :)
-
-void dataLinear(int n, int m, int gridArray[n+n-1][m+m-1]){
-
     
 }
 
 // this main is just to test the code.
-/* int main(){
+ int main(){
     /*
     int lineArray[ROWS][COLS] = {
         {2,0,2,0,2},
@@ -66,7 +63,7 @@ void dataLinear(int n, int m, int gridArray[n+n-1][m+m-1]){
         {0,2,0,2,0},
         {2,0,2,0,2}
     };*/ // the 2 represents the Xs.
-/*
+
     printf("enter grid size nxm");
     int n,m;
     scanf("%d %d",&n, &m);
@@ -78,6 +75,6 @@ void dataLinear(int n, int m, int gridArray[n+n-1][m+m-1]){
     scanf("%d %d %d %d",&row1,&col1,&row2,&col2);
 
 
-    addLineToArrayCOPY(n,m,gridArray,row1,row2,col1,col2);
+    addLineToArray(n,m,gridArray,row1,row2,col1,col2);
     return 0;
-} */
+} 
