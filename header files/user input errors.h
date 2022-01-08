db8fa7ccc0ec4e570e39b61ba3981fdf85ab8570
OmarTammam25 <omartammam25@gmail.com>
2022-01-08 21:51:55 +0200

    debugging

--- header files/user input errors.h
+++ header files/user input errors.h
@@ -1,6 +1,6 @@
 int checkInput(int row1, int col1, int row2, int col2, int num_row, int num_col){
     //correct input
-    if((row1 == row2 || abs(row1 - row2) == 1) && (col1 == col2 || abs(col1 - col2) == 1 ) && (row1 <= num_row) && (row2 <= num_row) && (col1 <= num_col) && (col2 <= num_col)){
+    if((row1 == row2 || abs(row1 - row2) == 1) && (col1 == col2 || abs(col1 - col2) == 1 ) && (row1 <= num_row) && (row2 <= num_row) && (col1 <= num_col) && (col2 <= num_col) && !(row1 == row2 && col1 == col2)  ){
         return 1;
 
     }else{
